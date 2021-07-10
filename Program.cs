103f6eca9f5941b795158eb11462283b270c6cbc
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-07-10 18:29:45 +0100

    Help menu

--- Program.cs
+++ Program.cs
@@ -7,10 +7,29 @@ namespace DMPackageManager.CLI {
         static void Main(string[] args) {
             // TODO: Assign somewhere as a cache
             string dmpm_version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
-            Console.WriteLine(String.Format("DMPM CLI - v{0}", dmpm_version));
-            if(args.Length == 0) {
+            Console.WriteLine(String.Format("[===] DMPM CLI - v{0} [===]", dmpm_version));
+            Console.WriteLine("");
+            if (args.Length == 0) {
                 Console.WriteLine("Run 'dmpm help' for a list of commands");
                 Environment.Exit(1); // This technically wasnt a valid run
+            }
+            // If we are here we have atleast 1 arg
+            switch(args[0]) {
+                // Help menu
+                case "help":
+                    Console.WriteLine("Commands:");
+                    Console.WriteLine("dmpm init - Initialise a project folder for use with DMPM");
+                    Console.WriteLine("dmpm add <package> - Add a package to the project");
+                    Console.WriteLine("dmpm remove <package> - Remove a package from the project");
+                    Console.WriteLine("dmpm check [package] - Check one (or all) package for updates");
+                    Console.WriteLine("dmpm update [package] - Updates one (or all) package to its latest version");
+                    Console.WriteLine("dmpm install - Installs all packages required by this project");
+                    Console.WriteLine("");
+                    Console.WriteLine("Key: <required_argument> [optional_argument]");       
+                    break;
+                default:
+                    Console.WriteLine("Unknown command. Run 'dmpm help' for a list of commands");
+                    break;
             }
         }
     }
