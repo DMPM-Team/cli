cd213be7b00940ad6779d76a80ada4de620ab0df
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2021-07-10 16:20:51 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace DMPackageManager.CLI {
+    class Program {
+        static void Main(string[] args) {
+            // TODO: Assign somewhere as a cache
+            string dmpm_version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            Console.WriteLine(String.Format("DMPM CLI - v{0}", dmpm_version));
+            if(args.Length == 0) {
+                Console.WriteLine("Run 'dmpm help' for a list of commands");
+            }
+        }
+    }
+}
